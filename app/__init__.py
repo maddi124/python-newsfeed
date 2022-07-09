f726519d1738af1046b2f3ed116586c6cada3957
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-07-09 00:01:51 -0400

    set up user logins and sessions

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 
@@ -11,9 +11,6 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
-  app.jinja_env.filters['format_url'] = filters.format_url
-  app.jinja_env.filters['format_date'] = filters.format_date
-  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   @app.route('/hello')
   def hello():
@@ -22,8 +19,11 @@ def create_app(test_config=None):
   # register routes
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  app.register_blueprint(api)
 
-
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   init_db(app)
 
--- app/models/User.py
+++ app/models/User.py
@@ -11,13 +11,23 @@ class User(Base):
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
   @validates('email')
   def validate_email(self, key, email):
     # make sure email address contains @ character
     assert '@' in email
+
     return email
+
   @validates('password')
   def validate_password(self, key, password):
     assert len(password) > 4
+
    # encrypt password
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+    
+  def verify_password(self, password):
+    return bcrypt.checkpw(
+      password.encode('utf-8'),
+      self.password.encode('utf-8')
+    )
